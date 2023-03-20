2b7cf8074ce9f7996e8af1bfe3db36707494bab3
강동현 <101819709+peterkang98@users.noreply.github.com>
2023-03-20 16:51:22 +0900

    third chapter commit

---
+++ spring-ch3/src/main/java/beans/Parrot.java
@@ -0,0 +1,20 @@
+package beans;
+
+import org.springframework.stereotype.Component;
+
+public class Parrot {
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  @Override
+  public String toString(){
+    return "Parrot : " + name;
+  }
+}
