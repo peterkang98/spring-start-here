85895bd5b68f071d7fc5bee0535a4be609b3b0ae
강동현 <101819709+peterkang98@users.noreply.github.com>
2023-03-24 23:32:45 +0900

    chapter seven commit

---
+++ spring-ch7/src/main/java/com/example/springch7/SpringCh7Application.java
@@ -0,0 +1,13 @@
+package com.example.springch7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCh7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCh7Application.class, args);
+	}
+
+}
---
+++ spring-ch7/src/test/java/com/example/springch7/SpringCh7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springch7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCh7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
